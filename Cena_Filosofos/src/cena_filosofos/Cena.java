16a7bca3e44ff531b6229f4466da61eed976e07c
Jpchapel <jpchapel82@gmail.com>
2020-01-27 19:07:52 +0100

    0.0

---
+++ Cena_Filosofos/src/cena_filosofos/Cena.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cena_filosofos;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Stream
+ */
+public class Cena extends Thread{
+    private final Filosofo filosofo;
+    private final int id;
+    
+    public Cena(int id, Filosofo filosofo){
+        this.id = id;
+        this.filosofo = filosofo;
+    }
+
+    @Override
+    public void run(){
+        while(true){
+            try {
+                
+                Thread.sleep((long) (Math.random() * 5000));
+                filosofo.comer(id);
+                Thread.sleep((long) (Math.random() * 5000));
+                filosofo.dejarDeComer(id);
+                
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Cena.class.getName()).log(Level.SEVERE, null, ex);
+            }           
+        }
+    }
+    
+}
---
+++ Cena_Filosofos/src/cena_filosofos/Cena_Filosofos.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cena_filosofos;
+
+/**
+ *
+ * @author Stream
+ */
+public class Cena_Filosofos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final int numeroFilosofos = 6;
+        
+        Filosofo filosofo = new Filosofo(numeroFilosofos);
+        
+        for (int i = 0; i < numeroFilosofos; i++) {
+            new Cena(i, filosofo).start();
+        }
+    }
+    
+}
